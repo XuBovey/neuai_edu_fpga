1fd466c2415d86eca37cbd56751ca0006d313eb7
xu.bovey <xu.bovey@foxmail.com>
2021-10-15 22:47:16 +0800

    spi rx data ok

--- neuai_demo_spi_slave/pin.tcl
+++ neuai_demo_spi_slave/pin.tcl
@@ -6,11 +6,9 @@ set_location_assignment PIN_L15 -to leds[4]
 set_location_assignment PIN_K12 -to leds[5]
 set_location_assignment PIN_L11 -to leds[6]
 set_location_assignment PIN_K11 -to leds[7]
-
 set_location_assignment PIN_J5 -to clk
-
 set_location_assignment PIN_B6 -to sck
 set_location_assignment PIN_A5 -to ssel
-set_location_assignment PIN_A7 -to mosi
-set_location_assignment PIN_B4 -to miso
+set_location_assignment PIN_A7 -to miso
+set_location_assignment PIN_B4 -to mosi
 
